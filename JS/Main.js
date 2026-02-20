af943925c04910dfa910ee5b9063e165bd4979ec
sajedegharibian <sajedegharibian@gmail.com>
2026-02-20 12:54:31 +0330

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -4,29 +4,23 @@ const paginationEl = document.querySelector(".pagination");
 const prevEl = document.querySelector(".prev");
 const nextEl = document.querySelector(".next");
 
-const totalPages = 5;
-let currentPage = 1;
-
 function renderPagination() {
   paginationEl.innerHTML = "";
   for (let i = 1; i <= totalPages; i++) {
     const li = document.createElement("li");
     li.innerText = i;
-
-    if (i === currentPage) {
-      li.classList.add("active");
-    }
-
-    li.addEventListener("click", () => {
-      currentPage = i;
-      updatePagination();
-    });
-
-    paginationEl.appendChild(li);
   }
 
-  prevEl.disabled = currentPage === 1;
-  nextEl.disabled = currentPage === totalPages;
+  if (i === currentPage) {
+    li.classList.add("active");
+  }
+
+  li.addEventListener("click", () => {
+    currentPage=i;
+    updatePagination();
+  });
+
+  paginationEl.appendChild(li)
 }
 
 function updatePagination() {
